77f86057edc4d4585d46bd77b17409d029a6c5bb
Rubem <rubekx@gmail.com>
2021-08-23 12:11:53 -0300

    Fix indentation

--- osticket/upload/include/class.validator.php
+++ osticket/upload/include/class.validator.php
@@ -1,262 +1,209 @@
 <?php
 /*********************************************************************
-class.validator.php
+    class.validator.php
 
-Input validation helper. This class contains collection of functions used for data validation.
+    Input validation helper. This class contains collection of functions used for data validation.
 
-Peter Rotich <peter@osticket.com>
-Copyright (c)  2006-2013 osTicket
-http://www.osticket.com
+    Peter Rotich <peter@osticket.com>
+    Copyright (c)  2006-2013 osTicket
+    http://www.osticket.com
 
-Released under the GNU General Public License WITHOUT ANY WARRANTY.
-See LICENSE.TXT for details.
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
 
-vim: expandtab sw=4 ts=4 sts=4:
- **********************************************************************/
+    vim: expandtab sw=4 ts=4 sts=4:
+**********************************************************************/
 
-class Validator
-{
+class Validator {
 
-    public $input = array();
-    public $fields = array();
-    public $errors = array();
+    var $input=array();
+    var $fields=array();
+    var $errors=array();
 
-    public function __construct($fields = null)
-    {
+    function __construct($fields=null) {
         $this->setFields($fields);
     }
-    public function setFields(&$fields)
-    {
+    function setFields(&$fields){
 
-        if ($fields && is_array($fields)):
-            $this->fields = $fields;
+        if($fields && is_array($fields)):
+            $this->fields=$fields;
             return (true);
         endif;
 
         return (false);
     }
 
-    public function validate($source, $userinput = true)
-    {
-        $this->errors = array();
-        //Check the input and make sure the fields are specified.
-        if (!$source || !is_array($source)) {
-            $this->errors['err'] = __('Invalid input');
-        } elseif (!$this->fields || !is_array($this->fields)) {
-            $this->errors['err'] = __('No fields set up');
-        }
 
+    function validate($source,$userinput=true){
+        $this->errors=array();
+        //Check the input and make sure the fields are specified.
+        if(!$source || !is_array($source))
+            $this->errors['err']=__('Invalid input');
+        elseif(!$this->fields || !is_array($this->fields))
+            $this->errors['err']=__('No fields set up');
         //Abort on error
-        if ($this->errors) {
+        if($this->errors)
             return false;
-        }
 
         //if magic quotes are enabled - then try cleaning up inputs before validation...
-        if ($userinput && function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
-            $source = Format::strip_slashes($source);
-        }
+        if($userinput && function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
+            $source=Format::strip_slashes($source);
 
-        $this->input = $source;
+
+        $this->input=$source;
 
         //Do the do.
-        foreach ($this->fields as $k => $field) {
-            if (!$field['required'] && !$this->input[$k]) //NOT required...and no data provided...
-            {
+        foreach($this->fields as $k=>$field){
+            if(!$field['required'] && !$this->input[$k]) //NOT required...and no data provided...
                 continue;
-            }
 
-            if ($field['required'] && !isset($this->input[$k]) || (!$this->input[$k] && $field['type'] != 'int')) { //Required...and no data provided...
-                $this->errors[$k] = $field['error'];
+            if($field['required'] && !isset($this->input[$k]) || (!$this->input[$k] && $field['type']!='int')){ //Required...and no data provided...
+                $this->errors[$k]=$field['error'];
                 continue;
             }
 
             //We don't care about the type.
-            if ($field['type'] == '*') {
-                continue;
-            }
+            if ($field['type'] == '*') continue;
 
             //Do the actual validation based on the type.
-            switch (strtolower($field['type'])):
-        case 'integer':
-        case 'int':
-            if (!is_numeric($this->input[$k])) {
-                    $this->errors[$k] = $field['error'];
-            } elseif ($field['min'] && $this->input[$k] < $field['min']) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'double':
-            if (!is_numeric($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'text':
-        case 'string':
-            if (!is_string($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'array':
-            if (!$this->input[$k] || !is_array($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'radio':
-            if (!isset($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'date': //TODO...make sure it is really in GNU date format..
-            if (strtotime($this->input[$k]) === false) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'time': //TODO...make sure it is really in GNU time format..
-            break;
-        case 'phone':
-        case 'fax':
-            if (!self::is_phone($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'email':
-            if (!self::is_email($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'url':
-            if (!self::is_url($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'password':
-            if (strlen($this->input[$k]) < 5) {
-                $this->errors[$k] = $field['error'] . ' ' . __('(Five characters min)');
-            }
-
-            break;
-        case 'username':
-            $error = '';
-            if (!self::is_username($this->input[$k], $error)) {
-                $this->errors[$k] = $field['error'] . ": $error";
-            }
-
-            break;
-        case 'zipcode':
-            if (!is_numeric($this->input[$k]) || (strlen($this->input[$k]) != 5)) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        case 'cpf':
-            if (!self::is_cpf($this->input[$k])) {
-                $this->errors[$k] = $field['error'];
-            }
-
-            break;
-        default: //If param type is not set...or handle..error out...
-            $this->errors[$k] = $field['error'] . ' ' . __('(type not set)');
+            switch(strtolower($field['type'])):
+            case 'integer':
+            case 'int':
+                if(!is_numeric($this->input[$k]))
+                     $this->errors[$k]=$field['error'];
+                elseif ($field['min'] && $this->input[$k] < $field['min'])
+                     $this->errors[$k]=$field['error'];
+                break;
+            case 'double':
+                if(!is_numeric($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'text':
+            case 'string':
+                if(!is_string($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'array':
+                if(!$this->input[$k] || !is_array($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'radio':
+                if(!isset($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'date': //TODO...make sure it is really in GNU date format..
+                if(strtotime($this->input[$k])===false)
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'time': //TODO...make sure it is really in GNU time format..
+                break;
+            case 'phone':
+            case 'fax':
+                if(!self::is_phone($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'email':
+                if(!self::is_email($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'url':
+                if(!self::is_url($this->input[$k]))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'password':
+                if(strlen($this->input[$k])<5)
+                    $this->errors[$k]=$field['error'].' '.__('(Five characters min)');
+                break;
+            case 'username':
+                $error = '';
+                if (!self::is_username($this->input[$k], $error))
+                    $this->errors[$k]=$field['error'].": $error";
+                break;
+            case 'zipcode':
+                if(!is_numeric($this->input[$k]) || (strlen($this->input[$k])!=5))
+                    $this->errors[$k]=$field['error'];
+                break;
+            case 'cpf':
+                if (!self::is_cpf($this->input[$k])) 
+                    $this->errors[$k] = $field['error'];        
+                break;
+            default://If param type is not set...or handle..error out...
+                $this->errors[$k]=$field['error'].' '.__('(type not set)');
             endswitch;
         }
-        return ($this->errors) ? (false) : (true);
+        return ($this->errors)?(FALSE):(TRUE);
     }
 
-    public function iserror()
-    {
-        return $this->errors ? true : false;
+    function iserror(){
+        return $this->errors?true:false;
     }
 
-    public function errors()
-    {
+    function errors(){
         return $this->errors;
     }
 
     /*** Functions below can be called directly without class instance.
-    Validator::func(var..);  (nolint) ***/
-    public static function is_email($email, $list = false, $verify = false)
-    {
+         Validator::func(var..);  (nolint) ***/
+    static function is_email($email, $list=false, $verify=false) {
         require_once PEAR_DIR . 'Mail/RFC822.php';
         require_once PEAR_DIR . 'PEAR.php';
         $rfc822 = new Mail_RFC822();
-        if (!($mails = $rfc822->parseAddressList($email)) || PEAR::isError($mails)) {
+        if (!($mails = $rfc822->parseAddressList($email)) || PEAR::isError($mails))
             return false;
-        }
 
-        if (!$list && count($mails) > 1) {
+        if (!$list && count($mails) > 1)
             return false;
-        }
 
         foreach ($mails as $m) {
-            if (!$m->mailbox) {
+            if (!$m->mailbox)
                 return false;
-            }
-
-            if ($m->host == 'localhost') {
+            if ($m->host == 'localhost')
                 return false;
-            }
-
         }
 
         // According to RFC2821, the domain (A record) can be treated as an
         // MX if no MX records exist for the domain. Also, include a
         // full-stop trailing char so that the default domain of the server
         // is not added automatically
-        if ($verify and !count(dns_get_record($m->host . '.', DNS_MX))) {
-            return 0 < count(dns_get_record($m->host . '.', DNS_A | DNS_AAAA));
-        }
+        if ($verify and !count(dns_get_record($m->host.'.', DNS_MX)))
+            return 0 < count(dns_get_record($m->host.'.', DNS_A|DNS_AAAA));
 
         return true;
     }
 
-    public static function is_valid_email($email)
-    {
+    static function is_valid_email($email) {
         global $cfg;
         // Default to FALSE for installation
         return self::is_email($email, false, $cfg && $cfg->verifyEmailAddrs());
     }
 
-    public static function is_phone($phone)
-    {
+    static function is_phone($phone) {
         /* We're not really validating the phone number but just making sure it doesn't contain illegal chars and of acceptable len */
-        $stripped = preg_replace("(\(|\)|\-|\.|\+|[  ]+)", "", $phone);
-        return (!is_numeric($stripped) || ((strlen($stripped) < 7) || (strlen($stripped) > 16))) ? false : true;
+        $stripped=preg_replace("(\(|\)|\-|\.|\+|[  ]+)","",$phone);
+        return (!is_numeric($stripped) || ((strlen($stripped)<7) || (strlen($stripped)>16)))?false:true;
     }
 
-    public static function is_url($url)
-    {
+    static function is_url($url) {
         //XXX: parse_url is not ideal for validating urls but it's ideal for basic checks.
-        return ($url && ($info = parse_url($url)) && $info['host']);
+        return ($url && ($info=parse_url($url)) && $info['host']);
     }
 
-    public static function is_ip($ip)
-    {
+    static function is_ip($ip) {
         return filter_var(trim($ip), FILTER_VALIDATE_IP) !== false;
     }
 
-    public static function is_username($username, &$error = '')
-    {
-        if (strlen($username) < 2) {
+    static function is_username($username, &$error='') {
+        if (strlen($username)<2)
             $error = __('Username must have at least two (2) characters');
-        } elseif (!preg_match('/^[\p{L}\d._-]+$/u', $username)) {
+        elseif (!preg_match('/^[\p{L}\d._-]+$/u', $username))
             $error = __('Username contains invalid characters');
-        }
-
         return $error == '';
     }
 
-    public static function is_cpf($cpf)
-    {
+    public static function is_cpf($cpf){
         return self::check_cpf($cpf);
     }
+
 
     /*
      * check_ip
@@ -265,12 +212,10 @@ class Validator
      * @credit - borrowed from Symfony project
      *
      */
-    public static function check_ip($ip, $ips)
-    {
+    public static function check_ip($ip, $ips) {
 
-        if (!Validator::is_ip($ip)) {
+        if (!Validator::is_ip($ip))
             return false;
-        }
 
         $method = substr_count($ip, ':') > 1 ? 'check_ipv6' : 'check_ipv4';
         $ips = is_array($ips) ? $ips : array($ips);
@@ -290,19 +235,16 @@ class Validator
      *
      * @credit - borrowed from Symfony project
      */
-    public static function check_ipv4($ip, $cidr)
-    {
+    public static function check_ipv4($ip, $cidr) {
 
         if (false !== strpos($cidr, '/')) {
             list($address, $netmask) = explode('/', $cidr, 2);
 
-            if ($netmask === '0') {
+            if ($netmask === '0')
                 return filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-            }
 
-            if ($netmask < 0 || $netmask > 32) {
+            if ($netmask < 0 || $netmask > 32)
                 return false;
-            }
 
         } else {
             $address = $cidr;
@@ -310,9 +252,9 @@ class Validator
         }
 
         return 0 === substr_compare(
-            sprintf('%032b', ip2long($ip)),
-            sprintf('%032b', ip2long($address)),
-            0, $netmask);
+                sprintf('%032b', ip2long($ip)),
+                sprintf('%032b', ip2long($address)),
+                0, $netmask);
     }
 
     /**
@@ -325,19 +267,15 @@ class Validator
      * @see https://github.com/dsp/v6tools
      *
      */
-    public static function check_ipv6($ip, $cidr)
-    {
+    public static function check_ipv6($ip, $cidr) {
 
-        if (!((extension_loaded('sockets') && defined('AF_INET6')) || @inet_pton('::1'))) {
+        if (!((extension_loaded('sockets') && defined('AF_INET6')) || @inet_pton('::1')))
             return false;
-        }
 
         if (false !== strpos($cidr, '/')) {
             list($address, $netmask) = explode('/', $cidr, 2);
-            if ($netmask < 1 || $netmask > 128) {
+            if ($netmask < 1 || $netmask > 128)
                 return false;
-            }
-
         } else {
             $address = $cidr;
             $netmask = 128;
@@ -345,9 +283,8 @@ class Validator
 
         $bytesAddr = unpack('n*', @inet_pton($address));
         $bytesTest = unpack('n*', @inet_pton($ip));
-        if (!$bytesAddr || !$bytesTest) {
+        if (!$bytesAddr || !$bytesTest)
             return false;
-        }
 
         for ($i = 1, $ceil = ceil($netmask / 16); $i <= $ceil; ++$i) {
             $left = $netmask - 16 * ($i - 1);
@@ -361,20 +298,17 @@ class Validator
         return true;
     }
 
-    public function process($fields, $vars, &$errors)
-    {
+    function process($fields,$vars,&$errors){
 
         $val = new Validator();
         $val->setFields($fields);
-        if (!$val->validate($vars)) {
-            $errors = array_merge($errors, $val->errors());
-        }
+        if(!$val->validate($vars))
+            $errors=array_merge($errors,$val->errors());
 
         return (!$errors);
     }
 
-    public static function check_cpf($cpf)
-    {
+    public static function check_cpf($cpf){
         $cpf = preg_replace('/[^0-9]/is', '', $cpf);
         if (!is_numeric($cpf)) {
             return false;
@@ -396,8 +330,7 @@ class Validator
         return true;
     }
 
-    public static function check_cpf_is_exist($cpf = null, $client_uid = null)
-    {
+    public static function check_cpf_is_exist($cpf = null, $client_uid = null){
         $cpf = $cpf == null ? self::get_cpf($client_uid) : $cpf;
         if (self::check_cpf($cpf)) {
             return array("cpf_exist" => 1, 'error' => 0, 'msg' => 'CPF válido');
@@ -405,8 +338,7 @@ class Validator
         return array("cpf_exist" => 0, 'error' => 1, 'msg' => 'CPF inválido');
     }
 
-    public static function get_cpf($client_uid)
-    {
+    public static function get_cpf($client_uid){
         if (is_numeric($client_uid)) {
             $answer_table = FORM_ANSWER_TABLE;
             $entry_table = FORM_ENTRY_TABLE;
@@ -419,3 +351,4 @@ class Validator
         return null;
     }
 }
+?>
